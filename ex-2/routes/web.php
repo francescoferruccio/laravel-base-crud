02fa387225e9346875392861e6f1d6b9de299232
Francesco <francesco.ferruccio88@gmail.com>
2020-06-18 16:02:05 +0200

    fixato overflow body

--- ex-2/routes/web.php
+++ ex-2/routes/web.php
@@ -14,7 +14,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'OminiController@index')->name('home');
 
-
 Route::get('/show/{id}', 'OminiController@show')->name('show');
 
 Route::get('/create', 'OminiController@create')->name('create');
